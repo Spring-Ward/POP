47d42a3f22dcfd6aa948af3f24043854ec62a3f4
Hecker200 <96758054+Hecker200@users.noreply.github.com>
2024-03-10 10:39:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Purplegod 🐔","OBUSSIN 🎩","pmmj 🫠","POP 🍿"];
 
 
 let currentIndex = 0;
